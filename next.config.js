8b0fc61355a4026df3564327627f00f032cb7694
Erick <erickmaycolhc@gmail.com>
2022-12-23 12:51:56 -0500

    Creamos archivos env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    HOST_NAME: process.env.HOST_NAME,
+    HOST_NAME_API: process.env.HOST_NAME_API,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
